68477f05216cb0d01ae9461fc086b41c9ecb4457
lelouch zhang <112810388+lelouchzhang@users.noreply.github.com>
2025-11-15 01:22:51 +0800

    Revert "Eventpage UI"

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,14 +1,17 @@
-import EventDetails from "@/components/EventDetails"
+import { notFound } from "next/navigation"
 import { Suspense } from "react"
 
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
-    const slug = params.then((p) => p.slug);
+    const { slug } = await params
+    const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+    const request = await fetch(`${BASE_URL}/api/events/${slug}`)
+    const { data } = await request.json()
 
-
+    if (!data) return notFound()
     return (
         <main>
             <Suspense fallback={<div>Loading...</div>}>
-                <EventDetails params={slug} />
+                <h1>{data.slug}</h1>
             </Suspense>
         </main>
     )
